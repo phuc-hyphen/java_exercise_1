ca710a6438c142d7618edfe6f1a5a160db0b0a04
LE huu phuc <huu-phuc.le@epita.fr>
2022-02-09 22:31:59 +0100

    launcher 2.0

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
+import static java.awt.Color.white;
+import static java.awt.SystemColor.text;
+
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Welcome to Java 101, What is your command");
         Scanner scanner = new Scanner(System.in);
-
         String text = scanner.nextLine();
-        if (!text.equals("quit")) {
+        while (!text.equals("quit")) {
             System.out.println("Unknown command");
+            text = scanner.nextLine();
         }
     }
 }
